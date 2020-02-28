75f1f52b29e04f2c54712705a1516e7c1560dd14
hunter-heidenreich <hunter.scott.heidenreich@gmail.com>
2020-02-28 13:03:46 -0500

    Update README instructions and index for database connection

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const mysql = require('mysql');
+const CONFIG = require('./config.json');
 
+
+// Set up basic app
 app = express();
-
 app.get('/', function(req, res) {
   res.send('Success!');
+});
+
+// Configure basic SQL connection
+let con = mysql.createConnection(CONFIG);
+
+con.connect(function(err) {
+  if (err) {
+    console.log('Error connection to database');
+  } else {
+    console.log('Successfully connected to database!');
+  }
 });
 
 port = 8080;
