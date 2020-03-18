be06ff4b8ff892ae5ba7cf980436491f8079c292
ddebellis <ddebellis88@aol.com>
2020-03-18 00:38:04 -0400

    Changed stats endpoint to key/data, uploaded pie/bar charts

--- frontend/app/components/BarGraph/index.js
+++ frontend/app/components/BarGraph/index.js
@@ -5,10 +5,38 @@
  */
 
 import React from "react";
-//import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import { BarChart } from "reaviz";
 
-const dataIn = [
+// function Bar(props){
+//   const {data} = props;
+
+//   return (
+//     <div style={{ margin: "55px", textAlign: "center" }}>
+//       <BarChart width={350} height={250} data={data} />
+//     </div>
+//   );
+// };
+
+// Bar.propTypes = [
+//     {
+//       key: "Dem",
+//       data: 283
+//     },
+//     {
+//       key: "Ind",
+//       data: 3
+//     },
+//     {
+//       key: "Rep",
+//       data: 258
+//     }
+//   ];
+
+// export default Bar;
+
+
+export const data = [
   {
     key: "Dem",
     data: 283
@@ -23,20 +51,8 @@ const dataIn = [
   }
 ];
 
-
-function Bar(dataIn){
-  console.log(dataIn)
-  return (
+export default () => (
     <div style={{ margin: "55px", textAlign: "center" }}>
-      <BarChart width={350} height={250} data={dataIn} />
+      <BarChart width={350} height={250} data={data} />
     </div>
   );
-};
-
-// export default () => (
-//     <div style={{ margin: "55px", textAlign: "center" }}>
-//       <BarChart width={350} height={250} data={data} />
-//     </div>
-//   );
-
-export default Bar(dataIn);
